c7d841a7d2817b2a28793419cf91d89d0be5ae5e
Lion Schellenberg <lion.schellenberg@mni.thm.de>
2024-04-12 16:25:50 +0200

    Issue #4 fixed.

--- script.js
+++ script.js
@@ -1,6 +1,64 @@
 const row = document.querySelector('.row');
 
 async function createPokemonCards() {
+ /* Creates the elements that are necessary in the "info-window-content" div in order 
+  to correctly display the additional information about the selected Pokemon. */
+  const infoWindow = document.getElementById("info-window-content");
+
+  const closingBtn = document.createElement("button");
+  closingBtn.textContent = "X";
+  closingBtn.id = "closing-button";
+
+  const pokemonName = document.createElement("h1");
+  pokemonName.id = "pokemon-name";
+
+  const pokemonImage = document.createElement("img");
+  pokemonImage.id = "pokemon-image";
+
+  infoWindow.appendChild(closingBtn);
+  infoWindow.appendChild(pokemonName);
+  infoWindow.appendChild(pokemonImage);
+
+  document.getElementById("closing-button").addEventListener('click', () =>{
+    document.getElementById("info-window-wrapper").style.display = "none";
+  })
+
+  const table = document.createElement("table");
+  infoWindow.appendChild(table);
+
+  const getTable = document.querySelector('table');
+
+  const template = `<tr>
+                    <th>Abilities</th>
+                    <td id="abilities"></td>
+                  </tr>
+                  <tr>
+                  <th>Held items</th>
+                  <td id="held_items"></td>
+                </tr>
+                <tr>
+                  <th>Type</th>
+                  <td id="type"></td>
+                </tr>
+                <tr>
+                  <th>Moves</th>
+                  <td id="moves"></td>
+                </tr>
+                <tr>
+                  <th>Weight</th>
+                  <td id="weight"></td>
+                </tr>
+                <tr>
+                  <th>Cry</th>
+                  <td>
+                    <audio controls src="" id="cries" style="height: 25px; width: 100%;"></audio>
+                  </td>
+                </tr>`;
+  getTable.innerHTML = template;
+
+
+
+
   for (let i = 1; i <= 1008; i++) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
     const data = await response.json();
@@ -54,9 +112,11 @@ async function createPokemonCards() {
     row.appendChild(card);
 
 
-    /*listens if a card has been clicked and then executes the code that opens 
+    /*Listens if a card has been clicked and then executes the code that opens 
     the window with further information about the Pokemon.*/
     card.addEventListener('click', () => {
+
+
       document.getElementById("pokemon-name").innerHTML = data.name;
       document.getElementById("pokemon-image").src = data.sprites.front_default;
       document.getElementById("abilities").innerHTML = data.abilities.map(ability => ability.ability.name).slice(0, 3).join(', ');
@@ -71,15 +131,11 @@ async function createPokemonCards() {
       document.getElementById("moves").innerHTML = data.moves.map(move => move.move.name).slice(0, 3).join(', ');
       document.getElementById("weight").innerHTML = data.weight/10+" kg";
       document.getElementById("cries").src = data.cries.latest;
-      document.getElementById("info-window-wrapper").style.display = "flex";
-    });
+      document.getElementById("info-window-wrapper").style.display = "flex"; 
+  });
   }
 }
 
 // Call the createPokemonCards() function to create the Pokemon cards
 createPokemonCards();
-
-function closeInfoWindow() {
-  document.getElementById("info-window-wrapper").style.display = "none";
-}
 
